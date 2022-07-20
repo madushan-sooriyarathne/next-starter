20fbfdb22d7f8bdbd0acfba4ed776250a5bab60e
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2022-07-20 18:29:34 +0530

    added some basic types

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,17 @@
 declare global {
   // All global types go here
+
+  interface Image {
+    src: string;
+    alt?: string;
+    blurUrl: string;
+  }
+
+  type Color = "light" | "dark" | "normal";
+
+  type Alignment = "left" | "center" | "right";
+
+  type InputChangeEvent = InputEvent<HTMLTextAreaElement | HTMLInputElement>;
 }
 
 export {};
