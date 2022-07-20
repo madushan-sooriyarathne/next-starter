b31b7af2a97ab6678b10e9724a16f7074fbf0343
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2022-07-20 17:15:45 +0530

    added _app.ts and _document.ts with styled-components ssr configs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+
+import theme from "@styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Venus Glory Cosmetics</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
