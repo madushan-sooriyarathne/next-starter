6651fa179ac58df8d33d04e8e831fe5a55d87b8a
Madushan Sooriyarathne <madushan.sooriyarathne@outlook.com>
2022-07-20 17:15:14 +0530

    added styled-components config and custom webpack build script

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    styledComponents: true
+  },
+  images: {
+    domains: [] // allow images to be loaded from other domains
+  },
+  webpack: (config, {isServer}) => {
+    if(isServer) {
+      // require(require("path").join(process.cwd(), "utils/process-static-files"))
+      // Run this script only on the server
+    }
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
